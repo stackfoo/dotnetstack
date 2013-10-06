0b70a4dd2919001712bb01fb974d681855dac704
James Pritz <james@jamespritz.com>
2013-10-05 20:53:44 -0400

    Add ILMerge Demo

---
+++ Lib/Impl/BoFoo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackFoo.Security;
+
+namespace StackFoo {
+    internal class BoFoo: IBoFoo {
+
+        public User LastUpdatedBy { get; set; }
+
+        IUser IBoFoo.LastUpdatedBy {
+            get { return this.LastUpdatedBy; }
+        }
+
+
+    }
+}
---
+++ Lib/Impl/Security/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackFoo.Security;
+
+namespace StackFoo.Security {
+    internal class User: IUser {
+
+        public int UserID {
+            get;
+            set;
+        }
+    }
+}
